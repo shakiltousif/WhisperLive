9704fa0d8e7eb40b9e7c0fe1f0fd557c35dff447
Shakil Ahamed <shakil.ahamed.tousif.800@gmail.com>
2024-11-25 22:03:14 +0600

    update dockerfile

--- run_server.py
+++ run_server.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', '-p',
                         type=int,
-                        default=443,
+                        default=9090,
                         help="Websocket port to run the server on.")
     parser.add_argument('--backend', '-b',
                         type=str,
